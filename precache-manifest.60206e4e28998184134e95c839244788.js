cf0e3b1f2bd62fe7364d832c9c9e9eec897ec01a
Angad Preet Singh <angadpreetmalhi@gmail.com>
2020-10-12 01:53:14 -0700

    Updates

---
+++ precache-manifest.60206e4e28998184134e95c839244788.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2666cc3d6002cb0a67b15c307d84da0e",
+    "url": "/react-netflix-app/index.html"
+  },
+  {
+    "revision": "f41d60e8c4a47e190862",
+    "url": "/react-netflix-app/static/css/main.37065f9a.chunk.css"
+  },
+  {
+    "revision": "4272c585292996cc502f",
+    "url": "/react-netflix-app/static/js/2.82e39738.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-netflix-app/static/js/2.82e39738.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f41d60e8c4a47e190862",
+    "url": "/react-netflix-app/static/js/main.a7f7eac7.chunk.js"
+  },
+  {
+    "revision": "bf2ff7f5a712240b3877",
+    "url": "/react-netflix-app/static/js/runtime-main.ce43b40a.js"
+  }
+]);
